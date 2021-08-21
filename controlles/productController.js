ddb5d7d3d315369ee4b9865ba909cd71d29f36fa
Raju <raju.raw.zoo@gmail.com>
2021-08-21 23:03:08 +0530

    added All Methods

--- controlles/productController.js
+++ controlles/productController.js
@@ -1,5 +1,5 @@
 const Product = require('../models/productModels')
-
+const {getPostData} = require('../utils')
 
 //@desc    Gets All Products
 //@route   GET /api/products
@@ -13,8 +13,6 @@ async function getProducts(req,res) {
 	}
 }
 
-
-
 //@Desc Get Product By Id
 //@route GET /api/product/:id
 async function getProductById(req,res,id) {
@@ -25,8 +23,6 @@ async function getProductById(req,res,id) {
 		 res.writeHead(404, {'Content-Type': 'application/json'})
 		 res.end()
 		}
-
-		
 		res.writeHead(200, {'Content-Type': 'application/json'})
 		res.end(JSON.stringify(products)) 
 
@@ -35,7 +31,78 @@ async function getProductById(req,res,id) {
 	}
 }
 
+// @Desc  Create a Product
+// @route POST /api/products
+async function createProduct(req,res) {
+	try {
+		const body = await getPostData(req)
+		const {name,description,price} = JSON.parse(body)
+		const product = {
+			name,
+			description,
+			price
+		}
+		const newProduct = await Product.create(product)
+		res.writeHead(201, { 'Content-Type': 'application/json' })
+		return res.end(JSON.stringify(newProduct))
+	} catch(error) {
+		console.log(error)
+	}
+}
+
+
+// @Desc Update a Product
+// @route PUT /api/products 
+async function updateProduct(req,res,id) {
+	try {
+
+		const product = await Product.findById(id)
+		if(!product) {
+			res.writeHead(404, {'Content-Type': 'application/json'})
+			res.end()
+		 }
+		else {
+			const body = await getPostData(req)
+			const {title,description,price} = JSON.parse(body)
+			const productData = {
+				title:title || product.title,
+				description: description || product.description,
+				price: price || product.price
+			}
+
+			const updProduct = await Product.update(id,productData)
+			res.writeHead(201, { 'Content-Type': 'application/json' })
+			return res.end(JSON.stringify(updProduct))
+		}
+	} catch(error) {
+		console.log(error)
+	}
+} 
+
+//@Desc Delete A Product
+//@route DELETE /api/products/id
+async function removeProduct(req,res,id) {
+	const products = await Product.findById(id)
+	try {
+		if(!products) {
+			res.writeHead(404, {'Content-Type': 'application/json'})
+			res.end(JSON.stringify({ message: 'Product Not Found' }))
+		   }
+	
+		   else {
+			   await Product.deleteProduct(id)
+			   res.writeHead(200, { 'Content-Type': 'application/json' })
+			return res.end(JSON.stringify())
+		   }
+	} catch (error) {
+		console.log(error)
+	}
+}
+
 module.exports = {
 	getProducts,
-	getProductById
+	getProductById,
+	createProduct,
+	updateProduct,
+	removeProduct
 }
--- models/productModels.js
+++ models/productModels.js
@@ -1,23 +1,58 @@
-const product = require('../product.json')
+let products = require('../data/product.json')
+const {v4: uuidv4 } = require('uuid')
+const { writeDataToFile } = require('../utils')
 
 
 // Find All Products
 function findAll() {
 	return new Promise((resolve,reject) => {
-	     resolve(product)
+		resolve(products)
 	})
 }
 
 //Find Product By ID
 function findById(id) {
 	return new Promise((resolve,reject) => {
-		const product = product.find((p) => p.id === id)
+		const product = products.find((p) => p.id === id)
 	     resolve(product)
+	})
+}
+
+
+//Create new Product
+function create(product) {
+	return new Promise((resolve,reject) => {
+		const newProduct = {id: uuidv4(), ...product}
+		products.push(newProduct)
+		writeDataToFile('./data/product.json', products)
+		resolve(newProduct)
+	})
+}
+
+//Update a Product
+function update(id,product) {
+	return new Promise((resolve,reject) => {
+		const index = products.findIndex((p)=> p.id === id)
+		products[index] = {id, ...product}
+		writeDataToFile('./data/product.json', products)
+		resolve(products[index])
+	})
+}
+
+//Delete a Product
+function deleteProduct(id) {
+	return new Promise((resolve,reject) => {
+		products = products.filter((p) => p.id !== id)
+		writeDataToFile('./data/product.json',products)
+		resolve()
 	})
 }
 
 
 module.exports = {
     findAll,
-	findById
+	findById,
+	create,
+	update,
+	deleteProduct
 }
